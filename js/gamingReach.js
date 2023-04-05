775b721123e7172e1b9d36d7f2caa5acb70fa684
kc2029 <kc2029@hw.ac.uk>
2023-04-05 22:59:11 +0100

    V1.01

--- js/gamingReach.js
+++ js/gamingReach.js
@@ -155,7 +155,7 @@ d3.csv(
           svg
             .selectAll(".groupbar:not(#" + name + ")")
             .transition()
-            .duration(500)
+            .duration(200)
             .style("opacity", 0);
         })
         .on("mouseleave", function (event, d) {
@@ -176,7 +176,7 @@ d3.csv(
     .attr("text-anchor", "middle")
     .style("font-size", "25px")
     .style("font-weight", "bold")
-    .text("Percentage of people who play games by age and gender group");
+    .text("Percentage of UK gamer who play games by age and gender group");
 
   // Y axis label
   svg
@@ -189,3 +189,116 @@ d3.csv(
     .attr("font-size", "20")
     .text("Percentage(%)");
 });
+//
+//Pie Chart
+//
+// set the dimensions and margins of the graph
+const width = 450,
+  height = 450,
+  margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width, height) / 2 - margin;
+
+// append the svg object to the div called 'my_dataviz'
+const svg = d3
+  .select("#gameReachPie")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", `translate(${width / 2},${height / 2})`);
+
+// Create dummy data
+const data = {
+  "6-10": 9,
+  "11-14": 9,
+  "15-24": 22,
+  "25-34": 20,
+  "35-44": 16,
+  "45-64": 23,
+  "65+": 1,
+};
+
+// set the color scale
+// set the color scale
+const color = d3
+  .scaleOrdinal()
+  .domain(["6-10", "11-14", "15-24", "25-34", "35-44", "45-64", "65+"])
+  .range(d3.schemeCategory10.slice(0, 7));
+
+// Compute the position of each group on the pie:
+const pie = d3
+  .pie()
+  .sort(null) // Do not sort group by size
+  .value((d) => d[1]);
+const data_ready = pie(Object.entries(data));
+
+// The arc generator
+const arc = d3
+  .arc()
+  .innerRadius(radius * 0.5) // This is the size of the donut hole
+  .outerRadius(radius * 0.8);
+
+// Another arc that won't be drawn. Just for labels positioning
+const outerArc = d3
+  .arc()
+  .innerRadius(radius * 0.9)
+  .outerRadius(radius * 0.9);
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll("allSlices")
+  .data(data_ready)
+  .join("path")
+  .attr("d", arc)
+  .attr("fill", (d) => color(d.data[0]))
+  .attr("stroke", "white")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7);
+
+// Add the polylines between chart and labels:
+svg
+  .selectAll("allPolylines")
+  .data(data_ready)
+  .join("polyline")
+  .attr("stroke", "white")
+  .style("fill", "none")
+  .attr("stroke-width", 1)
+  .attr("points", function (d) {
+    const posA = arc.centroid(d); // line insertion in the slice
+    const posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
+    const posC = outerArc.centroid(d); // Label position = almost the same as posB
+    const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
+    posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+    return [posA, posB, posC];
+  });
+
+// Add the polylines between chart and labels:
+svg
+  .selectAll("allLabels")
+  .data(data_ready)
+  .join("text")
+  .text((d) => d.data[0])
+  .attr("transform", function (d) {
+    const pos = outerArc.centroid(d);
+    const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+    pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+    return `translate(${pos})`;
+  })
+  .style("text-anchor", function (d) {
+    const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+    return midangle < Math.PI ? "start" : "end";
+  })
+  .style("fill", "white"); // add this line to change the text color to white
+
+//Title Label
+svg
+  .append("text")
+  .attr("x", width - 450)
+  .attr("y", margin.top / 2 - 60)
+  .attr("text-anchor", "middle")
+  .style("font-size", "16px")
+  .style("font-weight", "bold")
+  .style("fill", "white")
+  .text("EU gamer Demographic");
